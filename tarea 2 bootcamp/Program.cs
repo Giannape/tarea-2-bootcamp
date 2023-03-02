cf81005f0781651efb10ee94fd8e3d7466eafd77
Giannape <126131206+Giannape@users.noreply.github.com>
2023-03-02 15:35:20 -0400

    Add files via upload

---
+++ tarea 2 bootcamp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Ingrese sus datos");
+
+Console.WriteLine("ingresa tu nombre: ");
+string Nombre=Console.ReadLine();
+
+Console.WriteLine("ingresa tu apellido: ");
+string Apellido=Console.ReadLine();
+
+Console.WriteLine("ingresa tu edad: ");
+int edad=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("¿sabe programar? ");
+string respuesta=Console.ReadLine();
+
+
+string mensaje = ("Hola, "+Nombre +" "+ Apellido + ",tu edad es "+edad + " y "+respuesta+" sabes programar.");
+Console.WriteLine(mensaje);
+
+
+
+
+
+
